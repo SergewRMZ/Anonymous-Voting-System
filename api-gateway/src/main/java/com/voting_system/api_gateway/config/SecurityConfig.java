fd764945958eadb415db91c80e50d7db3e1cc4f4
Sergew_RMZ <serge15games@gmail.com>
2026-09-10 20:22:37 -0600

    Application.yml has been configured

---
+++ api-gateway/src/main/java/com/voting_system/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.voting_system.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration 
+@EnableWebFluxSecurity 
+public class SecurityConfig {
+    @Bean 
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .authorizeExchange(exchanges -> exchanges
+                .anyExchange().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtSpec -> {
+
+            }))
+            .build();
+    }
+}
